4fb8747825b9a395af9e4bd5ee268d01d32d40e5
Nihel-Ben-Amor <87191762+Nihel-Ben-Amor@users.noreply.github.com>
2021-07-30 15:37:08 +0100

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -95,25 +95,19 @@ class Enrollment(models.Model):
     rating = models.FloatField(default=5.0)
 
 class Question(models.Model):
-    course=models.ForeignKey(Course,on_delete=models.CASCADE)
-    question_text=models.CharField(max_length=200)
-    note=models.IntegerField(default=0)
-    def is_get_score(self, selected_ids):
-        all_answers = self.choice_set.filter(is_correct=True).count()
-        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-        if all_answers == selected_correct:
-            return True
-        else:
-          return False
-class Choice(models.Model):
+    lesson = models.ForeignKey(Lesson,on_delete=models.CASCADE)
+    quesion = models.TextField()
+    question_grade = models.FloatField()
 
-    course=models.ForeignKey(Course,on_delete=models.CASCADE)
+
+class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
-    choix_text=models.CharField(max_length=200)
+    text = models.TextField()
     is_correct = models.BooleanField()
 class Submission(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-    choices = models.ManyToManyField(Choice)        
+    choices = models.ManyToManyField(Choice)  
+    grade = models.FloatField(blank=True,null=True)
 # <HINT> Create a Question Model with:
     # Used to persist question content for a course
     # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
